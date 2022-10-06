0ef9daa141f1855539d4cbc72e9a8835de1661c7
KenDingel <DingelKen@gmail.com>
2022-10-06 16:27:36 -0400

    Flask Inital Server

--- server.py
+++ server.py
@@ -5,8 +5,19 @@ from pymongo import MongoClient
 # json library to handle json objects
 from bson.json_util import loads, dumps
 
+# Mongo Setup
 mongo_client = MongoClient("mongo")
 db = mongo_client["WebWizards"]
 
 users_collection = db["users"]
 users_id_collection = db["user_id"]
+
+# Flask Setup
+app = Flask(__name__)
+
+@app.route("/hello", methods = ["GET"])
+def hello():
+    return "Hello!!"
+
+if __name__ == "__main__":
+    app.run(debug=False, port=4200)
