c6d0a033e730cee3bad494393aa7e038bcbf67fd
karli-T <60751070+karli-T@users.noreply.github.com>
2022-10-12 21:52:13 -0400

    homepage frontend  #4

---
+++ home/home_functions.js
@@ -0,0 +1,22 @@
+function show_login() {
+    const form = document.getElementById("login_form");
+    const sign_in_form = document.getElementById("register_form");
+    const display = form.style.display
+
+    if(display == "none"){
+        form.style.display = "block"
+        sign_in_form.style.display = "none"
+    }
+}
+
+function show_signup() {
+    const form = document.getElementById("register_form");
+    const login_form = document.getElementById("login_form");
+
+    const display = form.style.display
+
+    if(display == "none"){
+        form.style.display = "block"
+        login_form.style.display = "none"
+    }
+}
